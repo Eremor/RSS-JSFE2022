80997e46d643e50f20886f2d8fd18e3666d09102
Eremor <artem.s13@gmail.com>
2022-07-06 11:51:37 +0300

    init: start project online-store

---
+++ online-store/webpack.config.js
@@ -0,0 +1,108 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const WebpackPrettierPlugin = require("webpack-prettier-plugin");
+
+const devServer = (isDev) =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          hot: true,
+          port: 8080,
+          static: {
+            directory: path.join(__dirname, "dist"),
+          },
+        },
+      };
+
+const esLintPlugin = (isDev) =>
+  !isDev
+    ? []
+    : [
+        new ESLintPlugin({
+          extensions: ["ts", "js"],
+        }),
+      ];
+
+const prettierPlugin = (isDev) =>
+  !isDev
+    ? []
+    : [
+        new WebpackPrettierPlugin({
+          extensions: [".js", ".ts"],
+          printWidth: 120,
+          tabWidth: 2,
+          useTabs: false,
+          semi: true,
+          singleQuote: true,
+          encoding: "utf-8",
+        }),
+      ];
+
+module.exports = ({ develop }) => ({
+  mode: develop ? "development" : "production",
+  devtool: develop ? "inline-source-map" : "source-map",
+  entry: {
+    main: "./src/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/[hash][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./public",
+        },
+      ],
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    ...esLintPlugin(develop),
+    ...prettierPlugin(develop),
+  ],
+  ...devServer(develop),
+});
