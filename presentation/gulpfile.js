9f788c176ea0e5b2ff51d9a02d4450d161979660
Eremor <artem.s13@gmail.com>
2022-08-10 09:58:21 +0300

    feat: add slides

--- presentation/gulpfile.js
+++ presentation/gulpfile.js
@@ -289,7 +289,7 @@ gulp.task('serve', () => {
     connect.server({
         root: root,
         port: port,
-        host: '0.0.0.0',
+        host: '127.0.0.1',
         livereload: true
     })
 
